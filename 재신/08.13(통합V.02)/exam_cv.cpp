b3126d69f099adfbb562fc72b75d1b7bfdf4ebdb
Jason <jsstar1207@kookmin.ac.kr>
2018-08-16 17:07:10 +0900

    gitignore 수정

--- 재신/08.13(통합V.02)/exam_cv.cpp
+++ 재신/08.13(통합V.02)/exam_cv.cpp
@@ -269,7 +269,7 @@ int passing_lane_check(unsigned char* srcBuf, int iw, int ih, unsigned char* out
 
   if(count[0] + count[1] + count[2] < 1000) return -1;
 
-  return count[0] > count[1] ? (count[0] > count[2] ? 0 : 2) : (count[1] > count[2] ? 1 : 2);
+  return (count[0] > count[2] ? 0 : 1);
 }
 
 
